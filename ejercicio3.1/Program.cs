98806ec8e304317e3ba6139c17ce7dcc115b240f
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 19:18:20 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ejercicio
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int numb1;
+            int numb2;
+
+            Console.WriteLine("Digite dos numeros y le diremos cual de los dos numero es el mayor");
+
+            Console.WriteLine("Digite el primer numero");
+            numb1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Digite el segundo numero");
+            numb2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (numb1 > numb2)
+            {
+                Console.WriteLine($"El numero mayor es* :{numb1}");
+            }
+            else if (numb2 > numb1)
+            {
+                Console.WriteLine($"El numero mayor es:{numb2}");
+            }
+
+
+
+        }
+    }
+}
+
